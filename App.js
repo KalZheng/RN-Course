ce06ff13d29812421ea52ddcde622000e8fa00a0
Kal Zheng <kal.zheng@inspire-dt.com>
2023-05-10 16:07:15 +0800

    add simple style and text

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.dummyText}>Another Piece of Text</Text>
+      </View>
+      <Text style={styles.dummyText}>Hello World!</Text>
+      <Button title="Tap me!" />
     </View>
   );
 }
@@ -17,4 +19,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 16, 
+    borderWidth: 2, 
+    borderColor: 'red', 
+    padding: 16
+  },
 });
+
